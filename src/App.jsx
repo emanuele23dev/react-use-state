ee3fb1da0a8112f89b9bda5366600c407354f5e7
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-15 16:28:45 +0100

    initial commit and add header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import AppHeader from "./components/AppHeader";
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+import reactlogo from "../assets/img/react.svg";
+
+export default function AppHeader() {
+  return (
+    <header>
+      <img src={reactlogo} alt="" />
+      <h1>React Use State</h1>
+    </header>
+  );
+}
