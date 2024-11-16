278c3eded0a51510fb0a4aefd6a0072355db8ce1
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-16 17:45:31 +0100

    add loop rotate to react logo

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,7 @@ import reactlogo from "../assets/img/react.svg";
 export default function AppHeader() {
   return (
     <header>
-      <img src={reactlogo} alt="" />
+      <img className="reactLogo" src={reactlogo} alt="" />
       <h1>React Use State</h1>
     </header>
   );
